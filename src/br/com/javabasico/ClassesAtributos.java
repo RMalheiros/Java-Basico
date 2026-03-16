9cbefd66f0ab2aeb45aaf2d239c2e849706b2c4d
Rafael Malheiros de Melo <C070324@rede.tst>
2026-03-16 18:17:38 -0300

    Introdução Orientação à Objetos

---
+++ src/br/com/javabasico/ClassesAtributos.java
@@ -0,0 +1,17 @@
+package br.com.javabasico;
+
+public class ClassesAtributos {
+
+    int Idade = 10;
+
+    public static void main(String[] args) {
+        // Atributos
+
+        // Objeto - Instância
+        ClassesAtributos Objeto = new ClassesAtributos();
+        Objeto.Idade = 20;
+        System.out.println("Idade: " + Objeto.Idade);
+
+
+    }
+}
---
+++ src/br/com/javabasico/ClassesEObjetos.java
@@ -0,0 +1,20 @@
+package br.com.javabasico;
+
+public class ClassesEObjetos {
+    //Objetos
+
+    //Atributos
+    int Idade = 3;
+    String Raca = "Pitbull";
+    double Peso = 8.5;
+    boolean Status = true;
+
+    public static void main(String[] args) {
+        //Instanciar
+        ClassesEObjetos MyObjeto = new ClassesEObjetos();
+
+        //Exibindo
+        System.out.println("Peso " + MyObjeto.Peso);
+    }
+}
+
