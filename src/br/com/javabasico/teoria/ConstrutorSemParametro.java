4b83c5551fe42ff6de49c81f5b0b4cdc38417891
Rafael Malheiros de Melo <C070324@rede.tst>
2026-08-04 13:41:36 -0300

    Alterando ConstrutorSemParametro

--- src/br/com/javabasico/teoria/ConstrutorSemParametro.java
+++ src/br/com/javabasico/teoria/ConstrutorSemParametro.java
@@ -5,13 +5,14 @@ public class ConstrutorSemParametro {
     String Nome;
     // Construtores
     public ConstrutorSemParametro(){
-        Nome = "Rafael Malheiros";
+        String Nome;
     }
     // Métodos
     public static void main(String[] args) {
 
     // Instanciar // Objetos
         ConstrutorSemParametro Obj = new ConstrutorSemParametro();
+        Obj.Nome = "Rafael";
         System.out.println(Obj.Nome);
 
 
